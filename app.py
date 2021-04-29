81e5c0136fea86ee8f8fb3dcc4311228fabca955
Gabriel <tamiozzo98@gmail.com>
2021-04-29 04:44:41 -0300

    Classe EstadoDao criada e  imagens adicionadas na pasta img

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
+import os
 
 app = Flask(__name__,template_folder=os.path.abspath('application/view/templates'),static_folder=os.path.abspath('application/view/static'))
+
+@app.route("/")
+def home():
+    return render_template("base.html")
---
+++ application/model/DAO/estado_dao.py
@@ -0,0 +1,6 @@
+from application.model.entity.estado import Estado
+from application.model.entity.noticia import Noticia
+
+class EstadoDAO:
+    def __init__(self):
+        pass
