6054d8daa4f9bd4ad702d64fe19dd696eeffd994
Gabriel <tamiozzo98@gmail.com>
2021-05-02 21:57:04 -0300

    projeto finalizado.

--- application/controller/noticia_controller.py
+++ application/controller/noticia_controller.py
@@ -3,7 +3,7 @@ from application.model.entity.noticia import Noticia
 from application.model.DAO.estado_dao import EstadoDAO
 from application import app
 
-@app.route("/video/<int:id>")
+@app.route("/noticia/<int:id>")
 def noticia(id: int):
     noticia = EstadoDAO().find_noticia_by_id(id)
     return render_template("noticia.html", noticia = noticia)
